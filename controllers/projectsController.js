429f099b5a04d4e7c7562d99276e63520811ef4c
unknown <shashankbs2410@gmail.com>
2025-10-15 02:13:19 +0530

    projects service fix

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -3,8 +3,7 @@ const Projects = require("../models/project");
 module.exports.getprojects = async (req, res, next) => {
   try {
     const data = await Projects.find();
-    res.json(data);
-    console.log(data);
+    res.status(200).json(data);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
